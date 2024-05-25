76d591daf5b1a585c56637e8dfd029393163e24a
Rahul Kathait <edendattox@gmail.com>
2024-05-25 12:03:00 +0530

    make checkout page responsive

--- src/routes/checkout/+page.svelte
+++ src/routes/checkout/+page.svelte
@@ -2,57 +2,81 @@
   import Button from "$lib/components/button.svelte";
 </script>
 
-<div class="flex p-10 flex-col gap-9">
+<div class="flex py-5 px-4 md:p-10 flex-col gap-9">
   <div class="flex w-full justify-between items-center">
     <span class="flex flex-col">
-      <h1 class="text-6xl text-primary">kino-tekina</h1>
-      <p class="text-4xl text-secondary">functional stuff</p>
+      <h1 class="text-2xl md:text-6xl text-primary">kino-tekina</h1>
+      <p class="text-lg md:text-4xl text-secondary">functional stuff</p>
     </span>
-    <p class="text-7xl text-primary">Checkout</p>
+    <p class="text-lg md:text-7xl text-primary">Checkout</p>
   </div>
 
-  <div class="flex flex-col w-9/12 gap-10">
+  <div class="flex flex-col w-full md:w-9/12 gap-6 md:gap-10">
     <div class="flex flex-col gap-4 w-full">
-      <h3 class="text-4xl text-textDGray">Your Details</h3>
+      <h3 class="text-base md:text-4xl text-textDGray">Your Details</h3>
       <div class="flex flex-col w-full gap-5">
-        <div class="flex gap-4 w-full">
+        <div class="flex flex-col md:flex-row gap-4 w-full">
           <span class="flex flex-col w-full gap-1">
             <h4 class="text-textLGray">First Name</h4>
-            <input class="h-14 w-full rounded-[12px] bg-bgInputGray" />
+            <input
+              class="w-full px-4 py-4 text-lg rounded-[12px] bg-bgInputGray"
+            />
           </span>
           <span class="flex flex-col w-full gap-1">
             <h4 class="text-textLGray">Last Name</h4>
-            <input class="h-14 w-full rounded-[12px] bg-bgInputGray" />
+            <input
+              class="w-full px-4 py-4 text-lg rounded-[12px] bg-bgInputGray"
+            />
           </span>
         </div>
-        <div class="flex gap-4 w-full">
+        <div class="flex flex-col md:flex-row gap-4 w-full">
           <span class="flex flex-col w-full gap-1">
-            <h4 class="text-textLGray">First Name</h4>
-            <input class="h-14 rounded-[12px] w-full bg-bgInputGray" />
+            <h4 class="text-textLGray">Email</h4>
+            <input
+              class="w-full px-4 py-4 text-lg rounded-[12px] bg-bgInputGray"
+            />
           </span>
           <span class="flex flex-col w-full gap-1">
-            <h4 class="text-textLGray">Last Name</h4>
-            <input class="h-14 rounded-[12px] v bg-bgInputGray" />
-          </span>
-        </div>
-        <div class="flex gap-4 w-full">
-          <span class="flex flex-col w-full gap-1">
-            <h4 class="text-textLGray">First Name</h4>
-            <input class="h-14 rounded-[12px] w-full bg-bgInputGray" />
-          </span>
-          <span class="flex flex-col w-full gap-1">
-            <h4 class="text-textLGray">Last Name</h4>
-            <input class="h-14 rounded-[12px] v bg-bgInputGray" />
+            <h4 class="text-textLGray">Phone</h4>
+            <input
+              class="w-full px-4 py-4 text-lg rounded-[12px] bg-bgInputGray"
+            />
           </span>
         </div>
-        <div class="flex gap-4 w-full">
+        <div class="flex flex-col md:flex-row gap-4 w-full">
+          <div class="flex w-full gap-2">
+            <span class="flex flex-col w-full gap-1">
+              <h4 class="text-textLGray">Country</h4>
+              <input
+                class="w-full px-4 py-4 text-lg rounded-[12px] bg-bgInputGray"
+              />
+            </span>
+            <span class="flex flex-col w-full gap-1">
+              <h4 class="text-textLGray">City</h4>
+              <input
+                class="w-full px-4 py-4 text-lg rounded-[12px] bg-bgInputGray"
+              />
+            </span>
+          </div>
           <span class="flex flex-col w-full gap-1">
-            <h4 class="text-textLGray">First Name</h4>
-            <input class="h-14 rounded-[12px] w-full bg-bgInputGray" />
+            <h4 class="text-textLGray">Zipcode</h4>
+            <input
+              class="w-full px-4 py-4 text-lg rounded-[12px] bg-bgInputGray"
+            />
+          </span>
+        </div>
+        <div class="flex flex-col md:flex-row gap-4 w-full">
+          <span class="flex flex-col w-full gap-1">
+            <h4 class="text-textLGray">Address</h4>
+            <input
+              class="w-full px-4 py-4 text-lg rounded-[12px] bg-bgInputGray"
+            />
           </span>
           <span class="flex flex-col w-full gap-1">
-            <h4 class="text-textLGray">Last Name</h4>
-            <input class="h-14 rounded-[12px] v bg-bgInputGray" />
+            <h4 class="text-textLGray">Address line-2</h4>
+            <input
+              class="w-full px-4 py-4 text-lg rounded-[12px] bg-bgInputGray"
+            />
           </span>
         </div>
       </div>
